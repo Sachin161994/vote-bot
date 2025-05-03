c0b23467e941beae604cf224af79047f37007011
Sachin Mondal <sachinmondal161994@gmail.com>
2025-05-03 22:09:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -10,7 +11,10 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
+# Define path to chromedriver and launch browser
+chrome_path = "/usr/bin/chromedriver"
+service = Service(executable_path=chrome_path)
+driver = webdriver.Chrome(service=service, options=options)
 
 try:
     driver.get("https://mycutebaby.in/contest/participant/68137e5c2ce41")
